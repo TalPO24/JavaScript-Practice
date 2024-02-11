18ced1bbd16e8035616c3599bc404328ec4f4ce7
TalPO24 <talpor212@gmail.com>
2024-02-11 20:15:56 +0200

    JavaScript Syntax

---
+++ Lesson 1/JavaScript Syntax.js
@@ -0,0 +1,42 @@
+//* JavaScript Syntax
+
+// How to create variables:
+var x;
+let y;
+
+// How to use variables:
+x = 5;
+y = 6;
+let z = x + y;
+
+//* JavaScript Values
+// The JavaScript syntax defines two types of values:
+
+// - Fixed values
+// - Variable values
+// Fixed values are called Literals.
+
+// Variable values are called Variables.
+
+//* JavaScript Literals
+// The two most important syntax rules for fixed values are:
+
+// 1. Numbers are written with or without decimals:
+10.5;
+
+1001;
+
+// 2. Strings are text, written within double or single quotes:
+("John Doe");
+
+("John Doe");
+
+//* JavaScript is Case Sensitive
+// All JavaScript identifiers are case sensitive.
+
+// The variables lastName and lastname, are two different variables:
+
+let lastname, lastName;
+lastName = "Doe";
+lastname = "Peterson";
+// JavaScript does not interpret LET or Let as the keyword let.
