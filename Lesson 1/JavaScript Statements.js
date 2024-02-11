f4480b263389dfe56025edaa33cc986bbb254c59
TalPO24 <talpor212@gmail.com>
2024-02-11 19:44:04 +0200

    first commit

---
+++ Lesson 1/JavaScript Statements.js
@@ -0,0 +1,46 @@
+//* JavaScript Statements
+
+let x, y, z; // Statement 1
+x = 5; // Statement 2
+y = 6; // Statement 3
+z = x + y; // Statement 4
+
+//* JavaScript Programs
+// A computer program is a list of "instructions" to be "executed" by a computer.
+
+// In a programming language, these programming instructions are called statements.
+
+// A JavaScript program is a list of programming statements.
+
+//* In HTML, JavaScript programs are executed by the web browser.
+
+//* JavaScript Statements
+// JavaScript statements are composed of:
+
+// Values, Operators, Expressions, Keywords, and Comments.
+
+// This statement tells the browser to write "Hello Dolly." inside an HTML element with id="demo":
+document.getElementById("demo").innerHTML = "Hello Dolly.";
+
+// The statements are executed, one by one, in the same order as they are written.
+
+//* JavaScript Keywords
+// JavaScript statements often start with a keyword to identify the JavaScript action to be performed.
+
+// Our Reserved Words Reference lists all JavaScript keywords.
+
+// Here is a list of some of the keywords you will learn about in this tutorial:
+
+// Keyword	             Description
+
+// var	                 Declares a variable
+// let	                 Declares a block variable
+// const	             Declares a block constant
+// if	                 Marks a block of statements to be executed on a condition
+// switch	             Marks a block of statements to be executed in different cases
+// for	                 Marks a block of statements to be executed in a loop
+// function	             Declares a function
+// return	             Exits a function
+// try	                 Implements error handling to a block of statements
+
+//* JavaScript keywords are reserved words. Reserved words cannot be used as names for variables.
