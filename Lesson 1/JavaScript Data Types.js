55606012f38fa98dc8d9278be66c8de18c381fa0
TalPO24 <talpor212@gmail.com>
2024-02-12 18:49:51 +0200

    JavaScript Data Types

---
+++ Lesson 1/JavaScript Data Types.js
@@ -0,0 +1,58 @@
+//* JavaScript Data Types
+// JavaScript Data Types
+// JavaScript has 8 Datatypes
+
+/*
+1. String
+2. Number
+3. Bigint
+4. Boolean
+5. Undefined
+6. Null
+7. Symbol
+8. Object
+*/
+
+// The Object Datatype
+// The object data type can contain:
+
+/*
+1. An object
+2. An array
+3. A date
+*/
+
+// Examples
+// Numbers:
+let length = 16;
+let weight = 7.5;
+
+// Strings:
+let color = "Yellow";
+let lastName = "Johnson";
+
+// Booleans
+let x = true;
+let y = false;
+
+// Object:
+const person = { firstName: "John", lastName: "Doe" };
+
+// Array object:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// Date object:
+const date = new Date("2022-03-25");
+
+//* JavaScript Objects
+// JavaScript objects are written with curly braces {}.
+
+// Object properties are written as name:value pairs, separated by commas.
+
+// Example
+const person = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 50,
+  eyeColor: "blue",
+};
