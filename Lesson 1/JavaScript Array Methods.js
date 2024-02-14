d966e6a39923f6d014b535fcf98d3e865eae07dd
TalPO24 <talpor212@gmail.com>
2024-02-14 13:04:01 +0200

    JavaScript Array Methods

--- Lesson 1/JavaScript Array Methods.js
+++ Lesson 1/JavaScript Array Methods.js
@@ -96,3 +96,69 @@ const myChildren = myGirls.concat(myBoys);
 //* Splicing and Slicing Arrays
 // The splice() method adds new items to an array.
 // The slice() method slices out a piece of an array.
+
+//* JavaScript Array splice()
+// The splice() method can be used to add new items to an array:
+
+// Example
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.splice(2, 0, "Lemon", "Kiwi");
+
+// The first parameter (2) defines the position where new elements should be added (spliced in).
+// The second parameter (0) defines how many elements should be removed.
+// The rest of the parameters ("Lemon" , "Kiwi") define the new elements to be added.
+
+// The splice() method returns an array with the deleted items:
+
+// Example
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.splice(2, 2, "Lemon", "Kiwi");
+
+//* Using splice() to Remove Elements
+// With clever parameter setting, you can use splice() to remove elements without leaving "holes" in the array:
+
+// Example
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.splice(0, 1);
+
+// The first parameter (0) defines the position where new elements should be added (spliced in).
+// The second parameter (1) defines how many elements should be removed.
+// The rest of the parameters are omitted. No new elements will be added.
+
+//* JavaScript Array toSpliced()
+// ES2023 added the Array toSpliced() method as a safe way to splice an array without altering the original array.
+
+// The difference between the new toSpliced() method and the old splice() method is that the new method creates a // new array, keeping the original array unchanged, while the old method altered the original array.
+
+// Example
+const months = ["Jan", "Feb", "Mar", "Apr"];
+const spliced = months.toSpliced(0, 1);
+
+//* JavaScript Array slice()
+// The slice() method slices out a piece of an array into a new array:
+
+// Example
+// Slice out a part of an array starting from array element 1 ("Orange"):
+
+const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+const citrus = fruits.slice(1);
+// Note
+// The slice() method creates a new array.
+// The slice() method does not remove any elements from the source array.
+
+// The slice() method can take two arguments like slice(1, 3).
+// The method then selects elements from the start argument, and up to (but not including) the end argument.
+// Example
+const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+const citrus = fruits.slice(1, 3);
+
+// If the end argument is omitted, like in the first examples, the slice() method slices out the rest of the array.
+// Example
+const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+const citrus = fruits.slice(2);
+
+//* Automatic toString()
+// JavaScript automatically converts an array to a comma separated string when a primitive value is expected.
+// This is always the case when you try to output an array.
+
+//* Note: All JavaScript objects have a toString() method
